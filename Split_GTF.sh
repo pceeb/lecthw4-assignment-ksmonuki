c1c7d20ea609dcadd320b5e59d61ae5c6a716a16
Keira Monuki <ksmonuki@gmail.com>
2019-04-24 17:22:00 -0700

    Homework Week 4

--- Split_GTF.sh
+++ Split_GTF.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-#Script to split data based on chromosome and saves the individual chromosome data in separate files.
+#Script to split data based on chromosome, create directories for each chromosome, and save the individual chromosome data in separate files within their respective directories.
 #Requires argument of input file: sh bash_script.sh input_file
 #Example: sh Split_GTF.sh hg19.gtf
 #NOTE: the script is specific to the chromosomes given in the hg19.gtf file (ie. chr2, chr21, chr3). You must edit the script to separate the data if using an input file different from hg19.gtf.
 
 for i in 2 3 21
-do 
-grep -P "chr${i}\t" $1 > chr${i}_gtf
+do
+mkdir chr${i}_gtf
+cp $1 chr${i}_gtf
+cd chr${i}_gtf 
+grep -P "chr${i}\t" $1 > chr${i}.gtf
+cd ../
 done
  
